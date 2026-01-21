59e4ebf70959ee8bf94effabd1aaabf38d35e2fc
Link4Electronics <link4electronics@gmail.com>
2026-01-21 03:21:25 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,8 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/info.exult.exult.svg
 export DESKTOP=/usr/share/applications/info.exult.exult.desktop
+export DEPLOY_OPENGL=1
+export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/exult
