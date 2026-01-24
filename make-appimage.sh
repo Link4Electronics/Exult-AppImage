4f9605c34129ce8aaf04a4dcf9d3c37d5adacfdb
Link4Electronics <link4electronics@gmail.com>
2026-01-23 21:12:55 -0300

    remove pipewire

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/scalable/apps/info.exult.exult.svg
 export DESKTOP=/usr/share/applications/info.exult.exult.desktop
 export DEPLOY_OPENGL=1
-export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/exult
